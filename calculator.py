51d48c2326db5618fa814790c53a5dcde24a2ca3
Nielotz <mati2806@poczta.fm>
2023-04-03 18:43:53 +0200

    Breaking change

--- calculator.py
+++ calculator.py
@@ -7,8 +7,8 @@ def mul(a: float, b: float):
     return a * b
 
 def div(a: float, b: float):
-    if b == 0:
-        raise ZeroDivisionError
+    # if b == 0:
+    #     raise ZeroDivisionError
     return a / b
 
 def avg(a: list):
